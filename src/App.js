00f909a547907495fc120a2fe70ffc69241fca4e
mkhy19 <mkhy88519@gmail.com>
2022-02-23 23:25:11 +0200

    add structures, components and first style

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+ 
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage/>}></Route>
+              <Route exact path="/exchanges" element={<Exchanges/>}></Route>
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies/>}></Route>
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails/>}></Route>
+              <Route exact path="/news" element={<News/>}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2022
+            <Link to="/">
+              Crypto App.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div> 
+    </div>
+    )
+}
+
+export default App
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+  return (
+    <div>
+        Crypto Details
+ 
+    </div>
+    )
+}
+
+export default CryptoDetails
